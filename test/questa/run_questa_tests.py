91a817e123bd29cde031109eb81abff398d30821
Mihael Kovac <mhlkv02@gmail.com>
2021-06-09 14:07:57 +0200

    Fix and add remaining

--- test/questa/run_questa_tests.py
+++ test/questa/run_questa_tests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import subprocess as sp
 import os
@@ -10,6 +10,8 @@ from queue import Queue, Empty
 from concurrent.futures import ThreadPoolExecutor
 from collections import defaultdict
 from enum import IntEnum
+import signal
+from threading import Timer
 
 CACHE_DIR = '.run_questa_tests_cache'
 # LOG_FILE_NAME = 'tests.log'
@@ -18,11 +20,13 @@ LOG_FORMAT = '{} - {}'
 LOG_DATE_FORMAT = '%d-%m-%Y %H:%M:%S'
 FAILED_CACHE = os.path.join(CACHE_DIR, 'failed')
 ALL_CACHE = os.path.join(CACHE_DIR, 'all')
+REMAINING_CACHE = os.path.join(CACHE_DIR, 'remaining')
 
 ERROR_PATTERN = re.compile('error\s+(\d+)', re.IGNORECASE)
 REGISTER_SETUP_FAIL_PATTERN = re.compile('\s*Registers\s*setup\s*failed.*', re.IGNORECASE)
-TEST_FAIL_PATTERN = re.compiile('Trace\s*failed', re.IGNORECASE)
+TEST_FAIL_PATTERN = re.compile('Trace\s*failed', re.IGNORECASE)
 
+TEST_TIMEOUT_SEC = 40 * 60
 
 class TerminalColors:
     HEADER = '\033[95m'
@@ -41,11 +45,13 @@ class RunCache:
         if args.no_cache:
             self.cache_test = lambda testname: None
             self.cache_failed_test = lambda testname: None
+            self.cache_remaining = lambda test_names: None
             self.close = lambda: None
         else:
             os.makedirs(CACHE_DIR, exist_ok=True)
             self.all_cache = open(ALL_CACHE, 'w')
             self.failed_cache = open(FAILED_CACHE, 'w')
+            self.remaining_cache = open(REMAINING_CACHE, 'w')
             self.cache_test = lambda testname : print(testname, file=self.all_cache)
             self.cache_failed_test = lambda testname : print(testname, file=self.failed_cache)
             self._files = [self.all_cache, self.failed_cache]
@@ -55,6 +61,13 @@ class RunCache:
                 self.failed_cache.close()
             
             self.close = lambda: _close()
+
+            def _cache_remaining(test_names: list):
+                for test_name in test_names:
+                    print(test_name.strip(), file=self.remaining_cache)
+            self.cache_remaining = _cache_remaining
+                
+
 
 class LogLevel(IntEnum):
     INFO  = 1
@@ -196,6 +209,12 @@ def read_pipes(p):
 
             yield (out, err)
 
+def signal_handler_factory(trace_names: list, cache: RunCache):
+    def _handler(sig, frame):
+        cache.cache_remaining(trace_names)
+        sys.exit(0)
+
+    return _handler
 
 def get_test_list(log, args):
     test_list_file = ''
@@ -205,9 +224,11 @@ def get_test_list(log, args):
         test_list_file = '../../verif/tests/test_lists/all_tests.txt'
     elif args.command == 'last_run' or args.command == 'last':
         if args.failed:
-            test_list_file = os.path.join(CACHE_DIR, 'failed')
+            test_list_file = FAILED_CACHE
+        elif args.remaining:
+            test_list_file = REMAINING_CACHE
         else:
-            test_list_file = os.path.join(CACHE_DIR, 'all')
+            test_list_file = ALL_CACHE
         
         if not os.path.exists(test_list_file):
             log.info('No run cached -- cannot use the "last/last_run" option')
@@ -216,8 +237,15 @@ def get_test_list(log, args):
     test_list = f.readlines()
     f.close()
 
+    if not test_list:
+        log.info('No tests to run with the specified option!')
+        sys.exit(2)
+
     return test_list_file, test_list
                 
+def test_it(test_list: list):
+    while(test_list):
+        yield test_list.pop(0)
 
 
 common = ArgumentParser(add_help=False)
@@ -241,7 +269,12 @@ common.add_argument('--no-cache',
                     action='store_true',
                     help='Do not save this run (will not be used when using the "last_run" command)')
 
-subparsers = parser.add_subparsers(title='Run options', required=True, description='Multiple ways of specifiying which tests you want to run.', dest='command')
+common.add_argument('--timeout',
+                    type=int,
+                    required=False,
+                    help='Specify (in minutes) how long can a test run before it times out. Default is 40 mionutes')
+
+subparsers = parser.add_subparsers(title='Run options', description='Multiple ways of specifiying which tests you want to run.', dest='command')
 
 
 list_parser = subparsers.add_parser('list',
@@ -259,17 +292,23 @@ last_run_parser = subparsers.add_parser('last_run',
                                         description='Run tests ran in the last run of the script',
                                         parents=[common])
 last_run_parser.add_argument('--failed', action='store_true', help='Run only the tests that failed last time')
+last_run_parser.add_argument('--remaining', action='store_true', help='Run the tests which should have been run, but the program was interrupted')
 
 parser.epilog = "--- Arguments common to all commands ---" + common.format_help().replace(common.format_usage(), '')
 
 args = parser.parse_args()
 
 log = Log(args)
+cache = RunCache(args)
+timeout = args.timeout * 60 if args.timeout else TEST_TIMEOUT_SEC
 
-nvdla_root    = args.nvdla_root
 test_folder = args.test_folder 
 
 test_list, tests = get_test_list(log, args)
+
+signal_handler = signal_handler_factory(tests, cache)
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
 
 log.debug('Running tests from test list: {} ({})'.format(test_list, os.path.abspath(test_list)))
 
@@ -279,15 +318,46 @@ make_error_dict  = defaultdict(list)
 return_code_dict = defaultdict(list)
 setup_register_list = []
 trace_fail_list = []
-cache = RunCache(args)
 
-for i, filename in enumerate(tests, start=1):
+for i, filename in enumerate(test_it(tests), start=1):
     filename = filename.strip()
-    
-    log.add_logfile(filename, LogLevel.DEBUG)
 
-    log.info('Building test: {}', filename)
-    sp.run(['/usr/bin/make', 'clean', 'all'], cwd=test_folder)
+    build_log = '{}_build'.format(filename)
+    test_log = '{}_test'.format(filename)
+    
+    log.add_logfile(build_log, LogLevel.DEBUG)
+
+    return_code = None
+
+    log.debug('Building test: {}'.format(filename))
+    with sp.Popen(['/usr/bin/make', 'trace={}'.format(filename), 'clean', 'all'],
+                  cwd=test_folder,
+                  stdout=sp.PIPE,
+                  stderr=sp.PIPE,
+                  text=True) as build_proc:
+
+        for out, err in read_pipes(build_proc): 
+            if out:
+                out = out.strip()
+                log.debug(out)
+            if err:
+                err = err.strip()
+                log.debug(err)
+
+                err_search_result = ERROR_PATTERN.search(err)
+                if err_search_result:
+                    log.debug('Test {} failed during build'.format(filename))
+
+                    
+        return_code = build_proc.poll()
+        if return_code is not None and return_code != 0:
+            log.debug('Test {} failed during build with return code: {}'.format(filename, return_code))
+
+
+  # sp.run(['/usr/bin/make', 'trace={}'.format(filename), 'clean', 'all'], cwd=test_folder)
+
+    log.remove_logfile(build_log)
+    log.add_logfile(test_log, LogLevel.DEBUG)
 
     log.test_pass_start('Running test ({}/{}): {} '.format(i, total_test_count, filename))
 
@@ -305,28 +375,42 @@ for i, filename in enumerate(tests, start=1):
                   cwd=test_folder,
                   text=True) as p:
 
+        def on_timeout():
+            log.debug('Test timed out!')
+            p.terminate()
+
+        timer = Timer(timeout, on_timeout)
+        timer.start()
+
         for out, err in read_pipes(p):
+
+            register_fail_result_out, register_fail_result_err = None, None
+            test_fail_result_out, test_fail_result_err = None, None
 
             if out:
                 out = out.strip()
                 log.debug(out)
-                
+
+                register_fail_result_out = REGISTER_SETUP_FAIL_PATTERN.search(out)
+                test_fail_result_out = TEST_FAIL_PATTERN.search(out)
+
             if err:
                 err = err.strip()
                 log.debug(err)
                 
                 error_search_result = ERROR_PATTERN.search(err)
                 register_fail_result_err = REGISTER_SETUP_FAIL_PATTERN.search(err)
-                register_fail_result_out = REGISTER_SETUP_FAIL_PATTERN.search(out)
                 test_fail_result_err = TEST_FAIL_PATTERN.search(err)
-                test_fail_result_out = TEST_FAIL_PATTERN.search(out)
 
                 if error_search_result:
                     test_failed_make = True
                     make_error = error_search_result.group(1)
                 
-                test_failed_setup_registers =  register_fail_result_err or register_fail_result_out:
+                test_failed_setup_registers =  register_fail_result_err or register_fail_result_out
                 test_failed_trace = test_fail_result_err or test_fail_result_out
+
+
+        timer.cancel()
 
 
         return_code = p.poll()
@@ -352,7 +436,7 @@ for i, filename in enumerate(tests, start=1):
 
     cache.cache_test(filename)
 
-    log.remove_logfile(filename)
+    log.remove_logfile(test_log)
 
 
 log.info('')
@@ -373,7 +457,7 @@ if args.log:
     
     setup_log_file = os.path.join(args.log, 'setup_registers.txt')
     log_err_list(log, setup_log_file,
-        'Saving tests which failed on registers setup to: {}'.format(setup_log_file), setup_log_file)
+        'Saving tests which failed on registers setup to: {}'.format(setup_log_file), setup_register_list)
 
     trace_err_log_file = os.path.join(args.log, 'trace_err.txt')
     log_err_list(log, trace_err_log_file,
